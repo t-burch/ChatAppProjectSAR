1f9ade24346a3c089b3e86e5826bed11b367038d
T. Burch <TBurchgart@hhek.de>
2023-08-18 10:22:49 +0200

    Added transceiver module

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import network.ClientDiscovery.broadcast
 import network.ClientDiscovery.listen
 import network.DiscoveredClientsStore
+import network.MessageTransceiver.listenForMessages
+import network.MessageTransceiver.sendMessage
 import util.Globals.EXPIRATION_TIME
 import kotlin.concurrent.thread
 
@@ -14,15 +16,22 @@ fun main(args: Array<String>) {
     val displayName = args[1]
     thread { listen() }
     thread { broadcast(interfaceName, displayName) }
+    thread { listenForMessages() }
 
     thread(start = true) {
         while (true) {
             Thread.sleep(EXPIRATION_TIME)
+            // TODO Set a stale flag instead of deleting
             DiscoveredClientsStore.removeStaleEntries()
 
             print("\u001b[H\u001b[2J")
             println(DiscoveredClientsStore.getAllClients())
         }
+    }
+
+    thread {
+        Thread.sleep(20000L)
+        sendMessage("TEST MESSAGE")
     }
 }
 
--- src/main/kotlin/network/MessageTransceiver.kt
+++ src/main/kotlin/network/MessageTransceiver.kt
@@ -1,4 +1,35 @@
 package network
 
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
 object MessageTransceiver {
+    private const val MESSAGE_PORT = 9999
+
+    fun listenForMessages() {
+        DatagramSocket(MESSAGE_PORT).use { socket ->
+            while (true) {
+                val receiveData = ByteArray(1024)
+                val packet = DatagramPacket(receiveData, receiveData.size)
+                socket.receive(packet)
+
+                val message = String(packet.data, 0, packet.length)
+                println("Received message: $message from ${packet.address.hostAddress}")
+            }
+        }
+    }
+
+    fun sendMessage(message: String) {
+        val targetAddress = DiscoveredClientsStore.getAllClients().keys.firstOrNull() ?: run{
+            println("No clients discovered to send message to!")
+            return
+        }
+
+        DatagramSocket().use{ socket ->
+            val sendData = message.toByteArray()
+            val packet = DatagramPacket(sendData, sendData.size, InetAddress.getByName(targetAddress), MESSAGE_PORT)
+            socket.send(packet)
+        }
+    }
 }
